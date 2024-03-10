6a3fb408ef068e286aca0a2b2b34fd1c6db89f69
cr0c0 <hyde69ciel@gmail.com>
2024-03-10 21:44:44 +0900

    #240310 파이썬 공부 - input

--- practice/test3.py
+++ practice/test3.py
@@ -1,2 +1,7 @@
-strValue = input('좋아하는 과일은 무엇인가요?')
-print(strValue + '을 좋아합니다.')
+# strValue = input('좋아하는 과일은 무엇인가요?')
+# print(strValue + '을 좋아합니다.')
+
+strId = input('아이디를 입력하세요.')
+strPw = input('비밀번호를 입력하세요.')
+print('입력한 ID는 ' + strId + '입니다.')
+print('입력한 비밀번호는 ' + strPw + '입니다.')
